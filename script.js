1dd93b6f56da94e576c8c5acb2b5401eb50f564c
IZLuisZI <leduardo0905@gmail.com>
2024-05-01 17:50:46 -0400

    Refactor resource menu event handling and update header styles

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-let resourcesElements = Array.from(
-  document.querySelectorAll("#Resources, #Versions")
-);
+// let resourcesElements = Array.from(
+//   document.querySelectorAll("#Resources, #Versions")
+// );
 
-resourcesElements.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    event.stopPropagation(); // Prevent the document click event from firing
-    let resourceMenu = element.querySelector(".resources-opened-menu");
+// resourcesElements.forEach((element) => {
+//   element.addEventListener("click", (event) => {
+//     event.stopPropagation(); // Prevent the document click event from firing
+//     let resourceMenu = element.querySelector(".resources-opened-menu");
 
-    let icons = element.querySelector(".icon");
+//     let icons = element.querySelector(".icon");
 
-    icons.style.rotate = icons.style.rotate === "180deg" ? "0deg" : "180deg";
+//     icons.style.rotate = icons.style.rotate === "180deg" ? "0deg" : "180deg";
 
-    resourceMenu.style.display =
-      resourceMenu.style.display === "block" ? "none" : "block";
-    console.log(resourceMenu.style.display);
-  });
-});
+//     resourceMenu.style.display =
+//       resourceMenu.style.display === "block" ? "none" : "block";
+//     console.log(resourceMenu.style.display);
+//   });
+// });
 
-// Add this event listener
-document.addEventListener("click", () => {
-  resourcesElements.forEach((element) => {
-    let resourceMenu = element.querySelector(".resources-opened-menu");
-    if (resourceMenu.style.display === "block") {
-      resourceMenu.style.display = "none";
-      let icons = element.querySelector(".icon");
-      icons.style.rotate = "0deg";
-    }
-  });
-});
+// // Add this event listener
+// document.addEventListener("click", () => {
+//   resourcesElements.forEach((element) => {
+//     let resourceMenu = element.querySelector(".resources-opened-menu");
+//     if (resourceMenu.style.display === "block") {
+//       resourceMenu.style.display = "none";
+//       let icons = element.querySelector(".icon");
+//       icons.style.rotate = "0deg";
+//     }
+//   });
+// });
 
 let hamMenu = document.querySelector(".ham-menu");
 
